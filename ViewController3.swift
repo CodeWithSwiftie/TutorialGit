474453be4f2a03fe2424882d8d1092bc91d8c2eb
Vladislav <vlad_tretyak07@icloud.com>
2023-05-19 20:22:47 +0300

    Сделал реализацию UILabel!

--- ViewController3.swift
+++ ViewController3.swift
@@ -2,6 +2,10 @@ class ViewController3; UIViewController {
     viewDidLoad() {
         super.viewDidLoad()
         // Все порешал!
+
+        // Добавил Label
+        let label = UILabel()
+        view.addSubview(label)
     }
 
     func viewWillApper() {
